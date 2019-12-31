a01ff5e5d8928fe9856d0f3edd39d3a551b89d2a
lDumBot <linuxaprentice@outlook.com>
2019-12-30 23:03:38 -0300

    a try for motivates mein 2020 and let me with think good about 2020 that not come but i already think about the shits that can be happen, i want a 2020 good for all

---
+++ 2020.hs
@@ -0,0 +1,2 @@
+new_year(x) =
+    putStrLn("You are very important,and don't judge you so much. So in " ++ x ++ " enjoy the present and don't mind with future! and give all you love without await for something.")
