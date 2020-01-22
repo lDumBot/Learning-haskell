4820abe25a36385f10101f39e63c1c6c577a2dc4
dumbot <linuxaprentice@outlook.com>
2020-01-21 23:19:38 -0300

    a file for pratice some concepts of the chapter.3

---
+++ exerc.hs
@@ -0,0 +1,5 @@
+main :: IO() --this is a file for i pratice the theory that i haven't revised,because the return of my classes
+main = do
+        putStrLn "i already had anxious anxiety crisis,cried,and i already had a existential crisis. "
+        putStr "But now i am good and i think that i will survive the 300 days of classes."
+        putStrLn "What bothers me now,is that i don't have time to program,and don't have time for study the theory"
